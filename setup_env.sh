7c2ac961c072ea5eb4072e3470677d9a1d4f25ed
Gustavo Corrente <gustavo.corrente@wisewaresolutions.com>
2015-06-24 20:25:29 +0100

    fix environment setup script
    Signed-off-by: Gustavo Corrente <gustavo.corrente@wisewaresolutions.com>

--- setup_env.sh
+++ setup_env.sh
@@ -4,22 +4,22 @@
 
 # When calling this script, an enviroment variable 'ROOTDIR' must be set
 # to contain a full path.
-if [ "ROOTDIR" == "" ]; then
+if [ "$ROOTDIR" == "" ]; then
   echo -e "
 ERROR: You must set ROOTDIR before calling this file.
    If you want to use this file without build.sh, then
    you could pass it on the same line like:
 
-      $ ROOTDIR=$(pwd) ./setup_env.sh
+      $ export ROOTDIR=$(pwd) ; source ./setup_env.sh
 
 "
   exit
 fi
 
 # Settings
-OUTDIR=${ROOTDIR}/output
-TOOLCHAIN_DIR=$OUTDIR/gcc-linaro-arm-linux-gnueabihf-4.8-2014.02_linux
-BUILDROOT_DIR=$OUTDIR/buildroot-2014.05
+export OUTDIR=${ROOTDIR}/output
+export TOOLCHAIN_DIR=$OUTDIR/gcc-linaro-arm-linux-gnueabihf-4.8-2014.02_linux
+export BUILDROOT_DIR=$OUTDIR/buildroot-2014.05
 
 
 export PATH=${TOOLCHAIN_DIR}/bin:$PATH
